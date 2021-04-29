d4b2b00395227958b3c7078ce469f046c279db08
Едуард Задорожній <eduard.zadorozhnsj20102002@gmail.com>
2021-04-29 20:15:56 +0300

    second commit

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,20 +1,22 @@
-import React, {PureComponent} from 'react';
-import TodoItem from './TodoItem';
+import React, { PureComponent } from 'react'
+import TodoItem from './TodoItem'
 
 class TodoList extends PureComponent {
-    render() {
-        const {list, removeTodo, toggleTodo} = this.props;
+  render () {
+    // eslint-disable-next-line react/prop-types
+    const { list, removeTodo, toggleTodo } = this.props
 
-        return(
+    return (
             <ul>
-                {	
+                {
+                    // eslint-disable-next-line react/prop-types
                     list.map((todo, i) => {
-                        return <TodoItem todo = {todo} key = {i} removeTodo = {removeTodo} toggleTodo = {toggleTodo} />
+                      return <TodoItem todo = {todo} key = {i} removeTodo = {removeTodo} toggleTodo = {toggleTodo} />
                     })
                 }
             </ul>
-        )
-    }
+    )
+  }
 }
 
 export default TodoList
